b6279c582d7b9161d865750f9b1f95b2ef20cced
Malav Adeshara <adesharamalav15124@gmail.com>
2025-03-16 11:41:32 +0530

    Some more Frontend code

--- Frontend/src/components/LocationSearchPanel.jsx
+++ Frontend/src/components/LocationSearchPanel.jsx
@@ -1,31 +1,35 @@
 import React from 'react'
 
-const LocationSearchPanel = () => {
+const LocationSearchPanel = (props) => {
+
+
+  const location = [
+    "24B, Near Kapoor's cafe, Sheryians Coding School, Bhopal",
+    "24B, Near Kapoor's cafe, Sheryians Coding School, Bhopal",
+    "24B, Near Kapoor's cafe, Sheryians Coding School, Bhopal",
+    "24B, Near Kapoor's cafe, Sheryians Coding School, Bhopal",
+  ];
+
   return (
     <div>
-      {/* This is just a sample data */}
-
-      <div className='flex gap-4 border-2 p-3 rounded-sm border-[#eee] active:border-black items-center justify-start my-2'> 
-        <h2 className='bg-[#eee] h-8 w-12 flex items-center justify-center rounded-full'><i className="ri-map-pin-2-fill"></i></h2>
-        <h4 className='font-medium'>24B, Near Kapoor's cafe, Sheryians Coding School, Bhopal</h4>
-      </div>
-
-      <div className='flex gap-4 border-2 p-3 rounded-sm border-[#eee] active:border-black items-center justify-start my-2'> 
-        <h2 className='bg-[#eee] h-8 w-12 flex items-center justify-center rounded-full'><i className="ri-map-pin-2-fill"></i></h2>
-        <h4 className='font-medium'>24B, Near Kapoor's cafe, Sheryians Coding School, Bhopal</h4>
-      </div>
-
-      <div className='flex gap-4 border-2 p-3 rounded-sm border-[#eee] active:border-black items-center justify-start my-2'> 
-        <h2 className='bg-[#eee] h-8 w-12 flex items-center justify-center rounded-full'><i className="ri-map-pin-2-fill"></i></h2>
-        <h4 className='font-medium'>24B, Near Kapoor's cafe, Sheryians Coding School, Bhopal</h4>
-      </div>
-
-      <div className='flex gap-4 border-2 p-3 rounded-sm border-[#eee] active:border-black items-center justify-start my-2'> 
-        <h2 className='bg-[#eee] h-8 w-12 flex items-center justify-center rounded-full'><i className="ri-map-pin-2-fill"></i></h2>
-        <h4 className='font-medium'>24B, Near Kapoor's cafe, Sheryians Coding School, Bhopal</h4>
-      </div>
+      {
+        location.map(function(element, index) {
+          return (
+            <div 
+            onClick={() => { 
+              props.setVehiclePanelOpen(true);
+              props.setpanelOpen(false); 
+            }} 
+            key={index}
+            className='flex gap-4 border-2 p-3 rounded-sm border-[#eee] active:border-black items-center justify-start my-2'>
+              <h2 className='bg-[#eee] h-8 w-12 flex items-center justify-center rounded-full'><i className="ri-map-pin-2-fill"></i></h2>
+              <h4 className='font-medium'>{element}</h4>
+            </div>
+          );
+        })
+      }
     </div>
-  )
+  );
 }
 
 export default LocationSearchPanel
